d2d2744c5d3287512ba2f9d490d9558a48fe821b
Liam Burnand <liamb1216@gmail.com>
2023-03-08 10:18:38 +0000

    Quick update of OrderSystem

--- core/src/com/oshewo/panic/tools/OrderSystem.java
+++ core/src/com/oshewo/panic/tools/OrderSystem.java
@@ -1,12 +1,11 @@
 package com.oshewo.panic.tools;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.oshewo.panic.enums.Product;
-import com.oshewo.panic.non_sprite.Recipe;
+import com.oshewo.panic.enums.*;
+import com.oshewo.panic.non_sprite.*;
 
 import java.util.*;
 
-import static com.oshewo.panic.screens.PlayScreen.currentOrder;
 import static com.oshewo.panic.screens.PlayScreen.orderHud;
 
 /**
@@ -15,35 +14,18 @@ import static com.oshewo.panic.screens.PlayScreen.orderHud;
  * @author sl3416, Oshewo
  */
 public class OrderSystem {
-//    public static HashMap<String, List<String>> recipes;
-//    public static Queue<Object> orders = new LinkedList<>();
-    private static final List<Recipe> orders = new ArrayList<>();
-//    private final int orderId;
+
+    private static final List<Customer> customers = new ArrayList<>();
     private com.badlogic.gdx.utils.Timer timer;
+    private final GameMode mode;
 
     /**
      * Instantiates a new Order system.
      * Creates recipes
      */
-    public OrderSystem() {
-//        recipes = new HashMap<>();
-//
-//        // burger
-//        recipes.put("Burger", Arrays.asList("bun, toasted", "beef, formed then fried"));
-//
-//        // salad
-//        recipes.put("Salad", Arrays.asList("lettuce, chopped", "tomato, chopped", "onion, chopped"));
-//        orderId = 1;
+    public OrderSystem(GameMode mode) {
+        this.mode = mode;
     }
-
-//    /**
-//     * Gets recipes.
-//     *
-//     * @return the recipes
-//     */
-//    public static HashMap<String, List<String>> getRecipes() {
-//        return recipes;
-//    }
 
     /**
      * Generate order randomly.
@@ -59,36 +41,24 @@ public class OrderSystem {
      */
     public void update() {
         timer = new com.badlogic.gdx.utils.Timer();
-        final Label recipeLabel = orderHud.getRecipeLabel();
-        final Label ingredient1Label = orderHud.getIngredient1Label();
-        final Label ingredient2Label = orderHud.getIngredient2Label();
-        final Label ingredient3Label = orderHud.getIngredient3Label();
-
+        final Label label = orderHud.getLabel();
         timer.scheduleTask(new com.badlogic.gdx.utils.Timer.Task() {
-
             @Override
             public void run() {
-                if (currentOrder != null) {
-                    recipeLabel.setText(currentOrder.getEndProduct());
 
-                    switch (currentOrder.getEndProduct()) {
-                        case "Burger":
-                            ingredient1Label.setText("bun");
-                            ingredient2Label.setText("patty");
-                            ingredient3Label.setText("");
-                            break;
-                        case "Salad":
-                            ingredient1Label.setText("lettuce");
-                            ingredient2Label.setText("tomato");
-                            ingredient3Label.setText("onion");
-                            break;
-                    }
-                } else {
-                    recipeLabel.setText("CONGRATULATIONS!");
-                    ingredient1Label.setText("You've completed");
-                    ingredient2Label.setText("level 1!");
-                    ingredient3Label.setText("");
-                }
+            }
+        }, 1);
+
+        timer.scheduleTask(new com.badlogic.gdx.utils.Timer.Task() {
+            @Override
+            public void run() {
+                StringBuilder sb = new StringBuilder();
+                if (customers.size() != 0)
+                    for (int i = 0; i < Math.min(3, customers.size()); i++)
+                        sb.append(customers.get(i).getOrder().getEndProduct().toString()).append("\n").append(customers.get(i).getOrder().getIngredients());
+                else
+                    sb.append("CONGRATULATIONS!").append("\n").append("    ").append("You've completed").append("\n").append("    ").append("level 1!");
+                label.setText(sb.toString());
             }
         }, 1);
     }
