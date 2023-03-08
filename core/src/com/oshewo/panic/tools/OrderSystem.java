513d2d50e926d1a2a02cc1bb698286e3ea1d03a0
Liam Burnand <liamb1216@gmail.com>
2023-03-08 15:51:06 +0000

    Things didn't really work out as well as I would have wanted, changed it to a thread

--- core/src/com/oshewo/panic/tools/OrderSystem.java
+++ core/src/com/oshewo/panic/tools/OrderSystem.java
@@ -1,6 +1,6 @@
 package com.oshewo.panic.tools;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.oshewo.panic.PiazzaPanic;
 import com.oshewo.panic.enums.*;
 import com.oshewo.panic.non_sprite.*;
 
@@ -16,15 +16,17 @@ import static com.oshewo.panic.screens.PlayScreen.orderHud;
 public class OrderSystem {
 
     private static final List<Customer> customers = new ArrayList<>();
-    private com.badlogic.gdx.utils.Timer timer;
-    private final GameMode mode;
+    private final PiazzaPanic game;
 
     /**
      * Instantiates a new Order system.
      * Creates recipes
      */
-    public OrderSystem(GameMode mode) {
-        this.mode = mode;
+    public OrderSystem(PiazzaPanic game) {
+        this.game = game;
+
+        CustomerCreator customerCreator = new CustomerCreator();
+        customerCreator.start();
     }
 
     /**
@@ -40,26 +42,70 @@ public class OrderSystem {
      * Updates order hud
      */
     public void update() {
-        timer = new com.badlogic.gdx.utils.Timer();
-        final Label label = orderHud.getLabel();
-        timer.scheduleTask(new com.badlogic.gdx.utils.Timer.Task() {
-            @Override
-            public void run() {
+        StringBuilder sb = new StringBuilder();
+        if (customers.size() != 0)
+            for (int i = 0; i < Math.min(3, customers.size()); i++)
+                sb.append(customers.get(i).getOrder().getEndProduct().toString()).append("\n").append(customers.get(i).getOrder().getIngredients()).append("\n");
+        else
+            sb.append("CONGRATULATIONS!").append("\n").append("    ").append("You've completed").append("\n").append("    ").append("level 1!");
+        orderHud.getLabel().setText(sb.toString());
+    }
 
-            }
-        }, 1);
+    private class CustomerCreator implements Runnable {
+        private Thread t;
 
-        timer.scheduleTask(new com.badlogic.gdx.utils.Timer.Task() {
-            @Override
-            public void run() {
-                StringBuilder sb = new StringBuilder();
-                if (customers.size() != 0)
-                    for (int i = 0; i < Math.min(3, customers.size()); i++)
-                        sb.append(customers.get(i).getOrder().getEndProduct().toString()).append("\n").append(customers.get(i).getOrder().getIngredients());
-                else
-                    sb.append("CONGRATULATIONS!").append("\n").append("    ").append("You've completed").append("\n").append("    ").append("level 1!");
-                label.setText(sb.toString());
+        CustomerCreator() {
+            if (game.isVerbose()) System.out.println("Starting CustomerCreator");
+        }
+
+        public void run() {
+            if (game.isVerbose()) System.out.println("Running CustomerCreator");
+            int time, customerNum;
+
+            switch (game.DIFFICULTY) {
+                case EASY:
+                    time = 60;
+                    customerNum = 3;
+                    break;
+                case MEDIUM:
+                    time = 45;
+                    customerNum = 5;
+                    break;
+                case HARD:
+                    time = 10;
+                    customerNum = 8;
+                    break;
+                default:
+                    System.out.println("Error difficulty");
+                    return;
             }
-        }, 1);
+
+            switch (game.MODE) {
+                case SCENARIO:
+                    for (int i = 0; i < customerNum; i++) {
+                        Customer customer = new Customer(generateOrder(), game.worldTimer);
+                        customers.add(customer);
+                        if (game.isVerbose()) System.out.println(customer);
+                        if (i + 1 == customerNum)
+                            break;
+                        try {
+                            Thread.sleep(time * 1000);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                case ENDLESS:
+                    break;
+            }
+            if (game.isVerbose()) System.out.println("Finished CustomerCreate");
+        }
+
+        public void start() {
+            if (t == null) {
+                t = new Thread(this);
+                t.start();
+            }
+        }
     }
 }
