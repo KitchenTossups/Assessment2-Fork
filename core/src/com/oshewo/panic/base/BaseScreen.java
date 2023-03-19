45db7fa9bd85ad69918085d252657155f7e8187e
Liam Burnand <liamb1216@gmail.com>
2023-03-19 02:57:17 +0000

    Changed the background colour and removed useless imports

--- core/src/com/oshewo/panic/base/BaseScreen.java
+++ core/src/com/oshewo/panic/base/BaseScreen.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.oshewo.panic.screens.PlayScreen;
 
 import java.util.Objects;
 
@@ -44,8 +43,10 @@ public abstract class BaseScreen implements Screen, InputProcessor {
         this.mainStage.act(dt);
 
         // render
-        Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT + GL20.GL_DEPTH_BUFFER_BIT);
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 
         // defined by user
         this.update(dt);
