5e29dd9119d10fe0797cd43ffa3f671b4503b6f2
Liam Burnand <liamb1216@gmail.com>
2023-03-19 03:01:16 +0000

    From sprite to actor, easier to keep on the screen and want to move away from batch

--- core/src/com/oshewo/panic/actor/Chef.java
+++ core/src/com/oshewo/panic/actor/Chef.java
@@ -2,11 +2,15 @@ package com.oshewo.panic.actor;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.oshewo.panic.base.BaseActor;
+import com.oshewo.panic.sprites.Food;
+
+import static com.oshewo.panic.sprites.Food.foodArray;
 //import com.eng1.non_actor.*;
 
 public class Chef extends BaseActor {
 
     private Object inventoryItem;
+    public boolean isHolding = false;
 
     public Chef(float x, float y, Stage s, int chefNumber) {
         super(x, y, s);
@@ -16,6 +20,22 @@ public class Chef extends BaseActor {
 
     public Object getInventoryItem() {
         return this.inventoryItem;
+    }
+
+    public Food nearestFood(float distance) {
+        Food nearestFood = null;
+        float nearestDistance = Float.MAX_VALUE;
+        for (Food food : foodArray) {
+            float currentDistance = food.getPosition().dst(this.getX(), this.getY());
+//             this.getPosition().dst(food.getPosition());
+            if (currentDistance <= distance && currentDistance < nearestDistance) {
+                System.out.println(currentDistance + " " + nearestDistance);
+                nearestDistance = currentDistance;
+                nearestFood = food;
+                System.out.println(nearestFood);
+            }
+        }
+        return nearestFood;
     }
 
 //    public void setInventoryItem(Object inventoryItem) {
