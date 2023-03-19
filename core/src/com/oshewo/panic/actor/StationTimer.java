005645b92f27fb1a1a851e5d870f6dce51824df6
Liam Burnand <liamb1216@gmail.com>
2023-03-19 02:39:17 +0000

    Changing the Countdown timer from sprite to actor, remade it to a self-contained class

---
+++ core/src/com/oshewo/panic/actor/StationTimer.java
@@ -0,0 +1,42 @@
+package com.oshewo.panic.actor;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.TimeUtils;
+import com.oshewo.panic.base.BaseActor;
+
+import java.util.Date;
+
+public class StationTimer extends BaseActor {
+
+    private final long timeStarted;
+    private final TimerSub timerSub;
+    private final int duration;
+
+    public StationTimer(float x, float y, int width, int height, Stage s, int seconds) {
+        super(x, y, s);
+        this.duration = seconds;
+        timeStarted = new Date().getTime();
+        timerSub = new TimerSub(x + 2, y + 2, width - 4, height - 4, s);
+        this.loadTexture("progressGrey.png", width, height);
+    }
+
+    public float getProgressPercent() {
+        return (float) TimeUtils.timeSinceMillis(timeStarted) / (duration * 1000);
+    }
+
+    public void delete() {
+        timerSub.remove();
+        this.remove();
+    }
+
+    public void setInnerWidth(float w) {
+        this.timerSub.setWidth(w);
+    }
+
+    static class TimerSub extends BaseActor {
+        public TimerSub(float x, float y, int width, int height, Stage s) {
+            super(x, y, s);
+            this.loadTexture("progressRed.png", width, height);
+        }
+    }
+}
