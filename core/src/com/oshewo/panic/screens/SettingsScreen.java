9db0b5133e453a4902be9a8cef5091e8c15f91b6
Liam Burnand <liamb1216@gmail.com>
2023-04-26 12:47:06 +0100

    Finished settings screen

--- core/src/com/oshewo/panic/screens/SettingsScreen.java
+++ core/src/com/oshewo/panic/screens/SettingsScreen.java
@@ -10,12 +10,14 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.oshewo.panic.*;
 import com.oshewo.panic.base.*;
+import com.oshewo.panic.enums.Difficulty;
+import com.oshewo.panic.enums.GameMode;
 
 public class SettingsScreen extends BaseScreen {
 
     private static final int buttonWidth = 125;
     private static final int buttonHeight = 50;
-    private final Label modeLabel;
+    private final Label modeLabel, difficultyLabel;
 
     public SettingsScreen(PiazzaPanic game) {
         BaseActor background = new BaseActor(0, 0, this.mainStage);
@@ -28,9 +30,10 @@ public class SettingsScreen extends BaseScreen {
         FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("Minecraftia-Regular.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter fontParameters = new FreeTypeFontGenerator.FreeTypeFontParameter();
         fontParameters.size = 20;
-        fontParameters.color = Color.BLACK;
+        fontParameters.color = Color.WHITE;
         fontParameters.borderColor = Color.BLACK;
         fontParameters.borderStraight = true;
+        fontParameters.borderWidth = 2;
         fontParameters.minFilter = Texture.TextureFilter.Linear;
         fontParameters.magFilter = Texture.TextureFilter.Linear;
 
@@ -43,6 +46,35 @@ public class SettingsScreen extends BaseScreen {
         this.modeLabel = new Label(String.format("The current game mode is: %s\nPress the button below to change it;", game.MODE), style);
         this.modeLabel.setAlignment(Align.center);
 
+        Label modeChangeLabel = new Label("CHANGE GAME MODE", style);
+        modeChangeLabel.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if (game.MODE == GameMode.SCENARIO)
+                    game.MODE = GameMode.ENDLESS;
+                else
+                    game.MODE = GameMode.SCENARIO;
+                modeLabel.setText(String.format("The current game mode is: %s\nPress the button below to change it;", game.MODE));
+            }
+        });
+
+        this.difficultyLabel = new Label(String.format("The current game difficulty is: %s\nPress the button below to change it;", game.DIFFICULTY), style);
+        this.difficultyLabel.setAlignment(Align.center);
+
+        Label difficultyChangeLabel = new Label("CHANGE GAME DIFFICULTY", style);
+        difficultyChangeLabel.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if (game.DIFFICULTY == Difficulty.EASY)
+                    game.DIFFICULTY = Difficulty.MEDIUM;
+                else if (game.DIFFICULTY == Difficulty.MEDIUM)
+                    game.DIFFICULTY = Difficulty.HARD;
+                else
+                    game.DIFFICULTY = Difficulty.EASY;
+                difficultyLabel.setText(String.format("The current game difficulty is: %s\nPress the button below to change it;", game.DIFFICULTY));
+            }
+        });
+
         TextButton.TextButtonStyle button = new TextButton.TextButtonStyle();
         button.font = game.labelStyle[0].font;
         button.up = skin.getDrawable("exit_button_inactive");
@@ -53,9 +85,15 @@ public class SettingsScreen extends BaseScreen {
         this.uiTable.pad(50);
         this.uiTable.row().height(300);
         this.uiTable.add(new Actor()); // Program will add a space if there is something here, even if it's empty
-        this.uiTable.row().height(100);
+        this.uiTable.row().height(50);
         this.uiTable.add(this.modeLabel).center().expandX().pad(10);
-        this.uiTable.row().height(100);
+        this.uiTable.row().height(50);
+        this.uiTable.add(modeChangeLabel).center().expandX().pad(10);
+        this.uiTable.row().height(50);
+        this.uiTable.add(this.difficultyLabel).center().expandX().pad(10);
+        this.uiTable.row().height(50);
+        this.uiTable.add(difficultyChangeLabel).center().expandX().pad(10);
+        this.uiTable.row().height(50);
         this.uiTable.add(buttonExit).center().size(buttonWidth, buttonHeight).pad(10);
 
         buttonExit.addListener(new ClickListener() {
