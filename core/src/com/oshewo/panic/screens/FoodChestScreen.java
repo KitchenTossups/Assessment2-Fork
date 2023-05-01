d6426300b5a4d392670285e7a39c4d601f6d096c
Jack-Camo <118351840+Jack-Camo@users.noreply.github.com>
2023-05-01 18:44:04 +0100

    Updated map to add text

--- core/src/com/oshewo/panic/screens/FoodChestScreen.java
+++ core/src/com/oshewo/panic/screens/FoodChestScreen.java
@@ -1,11 +1,14 @@
 package com.oshewo.panic.screens;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.oshewo.panic.PiazzaPanic;
 import com.oshewo.panic.base.BaseActor;
 import com.oshewo.panic.base.BaseScreen;
@@ -19,21 +22,28 @@ public class FoodChestScreen extends BaseScreen {
     private final PiazzaPanic game;
     private final TiledMap map;
     private final OrthoCachedTiledMapRenderer renderer;
+    private final OrthographicCamera gameCam;
+    private final Viewport gamePort;
 
     public FoodChestScreen(PiazzaPanic game){
         this.game = game;
-
+        this.gameCam = new OrthographicCamera(this.game.V_WIDTH, this.game.V_HEIGHT);
+        this.gamePort = new FitViewport(this.game.V_WIDTH, this.game.V_HEIGHT, this.gameCam);
         TmxMapLoader mapLoader = new TmxMapLoader();
         this.map = mapLoader.load("piazza-map-big2-chest-screen.tmx");
         this.renderer = new OrthoCachedTiledMapRenderer(this.map);
         this.renderer.render();
+        this.gameCam.position.set((this.game.V_WIDTH / 2), (this.game.V_HEIGHT / 2), 0); // 0,0 is apparently in the centre of the screen maybe...
+
         WorldCreator();
     }
 
 
 
     public void update(float dt) {
-
+        this.gameCam.update();
+        this.renderer.setView(this.gameCam);
+        this.renderer.render();
     }
 
     public void resizing(int width, int height) {
@@ -47,9 +57,6 @@ public class FoodChestScreen extends BaseScreen {
     private void WorldCreator() {
         for (MapLayer mapLayer : this.map.getLayers()) {
             switch (TiledAssets.getValueOf(mapLayer.getName())) {
-                case WALLS:
-                    InitialiseWalls(mapLayer);
-                    break;
                 case LETTUCE:
                     InitialiseFoodObject(mapLayer, Ingredients.LETTUCE);
                     break;
@@ -83,16 +90,6 @@ public class FoodChestScreen extends BaseScreen {
                 default:
                     break;
             }
-        }
-    }
-
-    private void InitialiseWalls(MapLayer mapLayer) {
-        for (RectangleMapObject object : mapLayer.getObjects().getByType(RectangleMapObject.class)) {
-            Rectangle rectangle = object.getRectangle();
-            BaseActor baseActor = new BaseActor(rectangle.x, rectangle.y, super.uiStage);
-            baseActor.setSize(rectangle.width, rectangle.height);
-            baseActor.setBoundaryRectangle();
-            walls.add(baseActor);
         }
     }
 
