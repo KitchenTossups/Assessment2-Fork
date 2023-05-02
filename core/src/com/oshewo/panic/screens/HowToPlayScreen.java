c8fa84b3ac995ad55ad24b4f10a17e68fa1434df
Liam Burnand <liamb1216@gmail.com>
2023-05-03 00:22:12 +0100

    Added how to play

---
+++ core/src/com/oshewo/panic/screens/HowToPlayScreen.java
@@ -0,0 +1,32 @@
+package com.oshewo.panic.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.oshewo.panic.PiazzaPanic;
+import com.oshewo.panic.base.*;
+
+public class HowToPlayScreen extends BaseScreen {
+
+    private final PiazzaPanic game;
+
+    public HowToPlayScreen(PiazzaPanic game) {
+        BaseActor background = new BaseActor(0, 0, this.mainStage);
+        background.loadTexture("loadscreenfinal.png");
+        background.setSize(game.V_WIDTH, game.V_HEIGHT);
+        this.game = game;
+    }
+
+    public void update(float dt) {
+        if (Gdx.input.isTouched()) {
+            this.dispose();
+            this.game.setActiveScreen(new MainMenu(this.game));
+        }
+    }
+
+    public void resizing(int width, int height) {
+
+    }
+
+    public void disposing() {
+
+    }
+}
